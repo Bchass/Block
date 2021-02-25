aedd4dfbc64a740bbb498916081397fab6afb27e
Bchass <brandonchasser22@gmail.com>
2021-02-24 23:21:08 -0500

    hopefully tabs are pretty now

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,7 @@ import json
 from time import time
 from uuid import uuid4
 
+#https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
 #creates empty lists for constructor
 class Blockchain(object):
   '''
@@ -32,38 +33,37 @@ def __init__(self):
   self.new_block(previous_hash = 1, proof = 100)
 	
 def new_block(self):
-		#parameters for each block that is created
-		block = {
+#parameters for each block that is created
+  block = {
 		'index': len(self.chain) + 1,
 		'timestamp': time(),
 		'transactions': self.current_transactions,
 		'proof': proof,
 		'previous_hash': previous_hash or self.hash(self.chain[-1]),
-		}
-		
+}
 		#reset the list of transactions
-		self.current_transactions = []
-		self.chain.append(block)
-		return block
+  self.current_transactions=[]
+  self.chain.append(block)
+  return block
   
   #add in information for current transactions
 def new_transaction(self):
-		self.current_transactions.append({
+  self.current_transactions.append({
 			'sender': test,
 			'recipient': test,
 			'amount': test,
-		})
+})
 
-		return self.last_block['index']+1
+  return self.last_block['index']+1
 
   #TODO: implement scrypt instead of sha256 after up and running
 @staticmethod
 def hash(block):
-		block_string = json.dumps(block,sort_keys=True).encode()
-		return hashlib.sha256(block_string).hexdigest()
+  block_string = json.dumps(block,sort_keys=True).encode()
+  return hashlib.sha256(block_string).hexdigest()
 	
 def last_block():
-		return self.chain[-1]
+  return self.chain[-1]
 
 
 	
