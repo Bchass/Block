c426bb64765bc88bfd5fae750550ef12a8f0d393
Bchass <brandonchasser22@gmail.com>
2021-02-28 17:59:38 -0500

    Scaled back to results of first block

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,11 @@
 import hashlib
 import json
-from time import time
+import time
 from uuid import uuid4
-from flask import Flask
-from API import app_page
+from flask import Flask, request
+#from API import app_page
 
-# TODO: Implement Scrypt after up and runnign with sha256
-# https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
-# creates empty lists for constructor
+#TODO: Move Flask to its own file and then add in mine and transaction functions. Implement Consenus algo for a decentralized network
 '''
 Proof algorithm:
 - find a number that is p, such that hash(pp) has 4 leading zeros and where p is the previous p
@@ -16,57 +14,86 @@ https://en.wikipedia.org/wiki/Hashcash?ref=hackernoon.com
 '''
 
 app = Flask(__name__)
-app.register_blueprint(app_page)
+#app.register_blueprint(app_page)
 node_identifer = str(uuid4()).replace('-','')
 
-class Blockchain(object):
+class Block:
+    def __init__(self,index,transactions,timestamp,previous_hash):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.nonce = 0
+
+    def hash(self):
+        block_string = json.dumps(self.__dict__,sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    #How hard with PoW algo will be
+    difficulty = 2
+
     def __init__(self):
+        self.unknown_transactions = []
         self.chain = []
-        self.current_transaction = []
+        self.genesis_block()
 
-        #genesis block, make sure there are no previous predecessors
-        self.new_block(previous_hash = 1, proof = 100)
+    #Make sure there aren't any predecessors before 0, genesis block starts at 0 and so does previous_hash which makes it a valid hash
+    def genesis_block(self):
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.hash()
+        self.chain.append(genesis_block)
 
-    #Validate the proof is correct
-    def proof(self,last_proof):
-        proof = 0
-        while self.valid_proof(last_proof,proof) is False:
-            proof += 1
-            return proof
-
-    def validate_proof(last_proof,proof):
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
-
-    #Params for each new block that is created    
-    def new_block(self,proof,previous_hash):
-        block = {
-        'index': len(self.chain) + 1,
-		'timestamp': time(),
-		'transactions': self.current_transactions,
-		'proof': proof,
-		'previous_hash': previous_hash or self.hash(self.chain[-1]), 
-        }
-
-        #Reset the list of transactions
-        self.current_transaction = []
-        self.chain.append(block)
-        return block
-    
-    #Add in information for current transactions
-    def new_transaction(self,sender,recipient,amount):
-        self.curremt_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount
-
-            })
-        return self.last_block['index']+1
-
-    def hash(block):
-        block_string = json.dumps(block,sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
-    def last_block(self):
+    #aka the last block
+    def straggler(self):
         return self.chain[-1]
+
+    def valid_proof(block, block_hash):
+        return(block_hash.startswith('0' * Blockchain.difficulty) and
+        block_hash == block.hash())
+
+    #Check to make sure proof is valid
+    def init_block(self,block,proof):
+        self.previous_hash = self.straggler.hash
+        if self.previous_hash != block.previous_hash:
+            return False
+        if not self.valid_proof(block,proof):
+            return False
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def proof(block):
+        block.nonce = 0
+        hash = block.hash()
+        while not hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            hash = block.hash()
+        return hash
+
+    def new_transactions(self,transaction):
+        self.unknown_transactions.append(transaction)
+
+    def mine(self):
+        if not self.unknown_transactions:
+            return False
+        straggler = self.straggler
+        new_block = Block(index = straggler.index+1,
+                          transactions=self.unknown_transactions,
+                          timestamp=time.time(),
+                          previous_hash=straggler.hash)
+        proof = self.proof(new_block)
+        self.init_block(new_block,proof)
+        self.unknown_transactions = []
+        return new_block.index
+
+blockchain = Blockchain()
+        
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({"length": len(chain_data),
+                       "chain": chain_data})
+
