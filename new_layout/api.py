3167fa7ce4f5f9be8b68b42046da532c5284023a
Bchass <brandonchasser22@gmail.com>
2021-03-24 21:22:04 -0400

    Added transaction

--- new_layout/api.py
+++ new_layout/api.py
@@ -1,5 +1,6 @@
 import blockchain
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
 
 blockchain_blueprint = Blueprint('blockchain_blueprint',__name__)
 
@@ -34,4 +35,17 @@ def mine_block():
     "previous_hash":block['previous_hash'],
   }
   return jsonify(response), 200
+
+# Retrieve all the transactions that have taken place
+@blockchain_blueprint.route('/transactions/new', methods=['POST'])
+def transacation():
+  values = request.get_json(force=True)
+
+  required = ['sender','recipient','amount']
+  if not all(x in values for x in required):
+    return 'Missing values',400
+
+  index = blockchain.bc.new_transactions(values['sender'],values['recipient'],values['amount'])
+  response = {'message': f'Transaction being added to Block {index}'}
+  return jsonify(response),201
 
