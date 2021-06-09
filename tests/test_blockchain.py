e3e178d8949a18d51f1fe49968c651ddb563c48f
Bchass <brandonchasser22@gmail.com>
2021-06-09 09:16:34 -0400

    Fixed tests, some are broken atm

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -1,20 +1,21 @@
 from blockchain import *
+from api import *
 import unittest
 
 class BlockchainTestCase(unittest.TestCase):
     def test_status(self):
-        self.blockchain = bc
-        self.blockchain = block
+        self.blockchain = Blockchain
+        self.blockchain = Block
     
-    def test_new_block(self,proof=1234, previous_hash='xyz'):
-        self.blockchain = bc.create_block(proof,previous_hash)
+    def test_new_block(self,proof=1234, previous_hash='xyz',miner=node_identifier):
+        self.blockchain = Blockchain.create_block(proof,previous_hash,miner)
 
     # Just to prove that the genesis block is randomized each time for a new chain
     def test_genesis_block(self):
-        self.blockchain = block.genesis_block = random.randint(0,100)
+        self.blockchain = Block.genesis_block = random.randint(0,100)
     
     def test_transactions(self, sender='x', recipient='y', amount=1):
-        self.blockchain = bc.new_transactions(
+        self.blockchain = Blockchain.new_transactions(
             sender=sender,
             recipient=recipient,
             amount=amount
@@ -23,7 +24,7 @@ class BlockchainTestCase(unittest.TestCase):
 class TestBlock(BlockchainTestCase):
     def test_block(self):
         self.test_new_block()
-        self.blockchain = bc
+        self.blockchain = Blockchain
         Lblock = self.blockchain.last_block
                 
         #TODO: Figure out previous_hash for Argon2
@@ -32,10 +33,11 @@ class TestBlock(BlockchainTestCase):
         assert Lblock['timestamp'] != 0
         assert Lblock['proof'] == 1234
         assert Lblock['previous_hash'] == 0
+        assert Lblock['miner'] == 1
 
     def test_new_transactions(self):
         self.test_transactions()
-        self.blockchain = bc
+        self.blockchain = Blockchain
         transaction = self.blockchain.current_transactions[-1]
         
         assert transaction
